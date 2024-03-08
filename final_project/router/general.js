f81a4086ec91d475fde98c7b28eba0b5d09f9c7c
Your Name <tomy123abc@gmail.com>
2024-03-07 19:00:18 -0800

    Task 4 to 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -42,14 +42,28 @@ public_users.get("/author/:author", function (req, res) {
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const title = req.params.title;
+  let books_by_title = {};
+  for (let book of Object.values(books)) {
+    if (book.title === title) {
+      books_by_title[book.title] = book;
+    }
+  }
+  if (Object.keys(books_by_title).length > 0) {
+    return res.send(books_by_title);
+  } else {
+    return res.status(404).json({ message: "No books found for this title" });
+  }
 });
 
 //  Get book review
 public_users.get("/review/:isbn", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const isbn = req.params.isbn;
+  if (isbn > 0 && isbn <= Object.keys(books).length) {
+    return res.send(books[isbn - 1].reviews);
+  } else {
+    return res.status(404).json({ message: "Invalid ISBN" });
+  }
 });
 
 module.exports.general = public_users;
